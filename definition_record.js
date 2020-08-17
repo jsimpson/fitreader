0b38332392821a7b84d22465c63378984893d71c
Jonathan Simpson <jsimpson.github@gmail.com>
2020-08-16 22:39:01 -0700

    extract def record

---
+++ definition_record.js
@@ -0,0 +1,50 @@
+import { FIELDS } from "./fields.js";
+
+import { DevFieldDefinition } from "./dev_field_definition.js";
+import { FieldDefinition } from "./field_definition.js";
+
+export class DefinitionRecord {
+  constructor(io, localNum, devFieldDefs = null) {
+    this.localNum = localNum;
+
+    this.reserved = io.readUint8();
+    this.architecture = io.readUint8();
+    const littleEndian = this.architecture === 0;
+    this.globalMsgNum = io.readUint16(littleEndian);
+    const numFields = io.readUint8();
+
+    this.fieldDefinitions = new Array(numFields);
+    for (let i = 0; i < numFields; i++) {
+      this.fieldDefinitions[i] = new FieldDefinition(io);
+    }
+
+    if (devFieldDefs !== null) {
+      const numDevFields = io.readUint8();
+      this.devFieldDefs = new Array(numDevFields);
+      for (let i = 0; i < numDevFields; i++) {
+        this.devFieldDefs[i] = new DevFieldDefinition(io, devFieldDefs);
+      }
+    }
+
+    this.dataRecords = [];
+  }
+
+  isLittleEndian() {
+    return this.architecture === 0;
+  }
+
+  valid() {
+    const fields = FIELDS[this.globalMsgNum];
+    if (fields === undefined) {
+      return [];
+    }
+
+    return this.dataRecords.map((dataRecord) => {
+      return dataRecord.valid().filter((dr) => {
+        if (dr[0] in fields) {
+          return dr;
+        }
+      });
+    });
+  }
+}
--- mod.js
+++ mod.js
@@ -5,8 +5,7 @@ import { FIELDS } from "./fields.js";
 import { MESSAGES } from "./messages.js";
 import { TYPES } from "./types.js";
 
-import { DevFieldDefinition } from "./dev_field_definition.js";
-import { FieldDefinition } from "./field_definition.js"
+import { DefinitionRecord } from "./definition_record.js";
 import { FileHeader } from "./file_header.js";
 
 import { readBit, readBits } from "./bits.js";
@@ -21,52 +20,6 @@ function main() {
   const fit = new Fit(io);
 
   console.log({ fit });
-}
-
-class DefinitionRecord {
-  constructor(io, localNum, devFieldDefs = null) {
-    this.localNum = localNum;
-
-    this.reserved = io.readUint8();
-    this.architecture = io.readUint8();
-    const littleEndian = this.architecture === 0;
-    this.globalMsgNum = io.readUint16(littleEndian);
-    const numFields = io.readUint8();
-
-    this.fieldDefinitions = new Array(numFields);
-    for (let i = 0; i < numFields; i++) {
-      this.fieldDefinitions[i] = new FieldDefinition(io);
-    }
-
-    if (devFieldDefs !== null) {
-      const numDevFields = io.readUint8();
-      this.devFieldDefs = new Array(numDevFields);
-      for (let i = 0; i < numDevFields; i++) {
-        this.devFieldDefs[i] = new DevFieldDefinition(io, devFieldDefs);
-      }
-    }
-
-    this.dataRecords = [];
-  }
-
-  isLittleEndian() {
-    return this.architecture === 0;
-  }
-
-  valid() {
-    const fields = FIELDS[this.globalMsgNum];
-    if (fields === undefined) {
-      return [];
-    }
-
-    return this.dataRecords.map((dataRecord) => {
-      return dataRecord.valid().filter((dr) => {
-        if (dr[0] in fields) {
-          return dr;
-        }
-      });
-    });
-  }
 }
 
 class RecordHeader {
