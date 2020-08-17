f7fa90c90ba53ef0c0491fac71122f7738158c0e
Jonathan Simpson <jsimpson.github@gmail.com>
2020-08-16 22:40:06 -0700

    extract record header

--- mod.js
+++ mod.js
@@ -7,8 +7,7 @@ import { TYPES } from "./types.js";
 
 import { DefinitionRecord } from "./definition_record.js";
 import { FileHeader } from "./file_header.js";
-
-import { readBit, readBits } from "./bits.js";
+import { RecordHeader } from "./record_header.js";
 
 function main() {
   const filename = Deno.args[0];
@@ -20,39 +19,6 @@ function main() {
   const fit = new Fit(io);
 
   console.log({ fit });
-}
-
-class RecordHeader {
-  constructor(io) {
-    const byte = io.readUint8();
-
-    this.headerType = readBit(byte, 7);
-    if (this.headerType === 0) {
-      this.messageType = readBit(byte, 6);
-      this.messageTypeSpecific = readBit(byte, 5);
-      this.reserved = readBit(byte, 4);
-      this.localMesssageType = readBits(byte, [3, 0]);
-    } else {
-      this.localMesssageType = readBits(byte, [6, 5]);
-      this.timeOffset = readBits(byte, [4, 0]);
-    }
-  }
-
-  isDefinition() {
-    return (this.headerType === 0) && (this.messageType == 1);
-  }
-
-  isData() {
-    return (this.headerType === 0) && (this.messageType == 0);
-  }
-
-  hasDevDefs() {
-    return (this.messageTypeSpecific === 1);
-  }
-
-  hasTimestamp() {
-    return (this.headerType === 1);
-  }
 }
 
 class DataField {
---
+++ record_header.js
@@ -0,0 +1,34 @@
+import { readBit, readBits } from "./bits.js";
+
+export class RecordHeader {
+  constructor(io) {
+    const byte = io.readUint8();
+
+    this.headerType = readBit(byte, 7);
+    if (this.headerType === 0) {
+      this.messageType = readBit(byte, 6);
+      this.messageTypeSpecific = readBit(byte, 5);
+      this.reserved = readBit(byte, 4);
+      this.localMesssageType = readBits(byte, [3, 0]);
+    } else {
+      this.localMesssageType = readBits(byte, [6, 5]);
+      this.timeOffset = readBits(byte, [4, 0]);
+    }
+  }
+
+  isDefinition() {
+    return (this.headerType === 0) && (this.messageType == 1);
+  }
+
+  isData() {
+    return (this.headerType === 0) && (this.messageType == 0);
+  }
+
+  hasDevDefs() {
+    return (this.messageTypeSpecific === 1);
+  }
+
+  hasTimestamp() {
+    return (this.headerType === 1);
+  }
+}
