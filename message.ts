07d50a95f3fd0fdfbdba3cb2590d493cba001b86
Jonathan Simpson <jsimpson.github@gmail.com>
2020-10-14 21:52:13 -0700

    properly process all datafield values...

--- message.ts
+++ message.ts
@@ -30,7 +30,7 @@ export class Message {
   ): { [index: string]: number | string }[] {
     const finished: { [index: string]: number | string }[] = [];
     definition.valid().map((dataRecords: [number, DataField][]) => {
-      const obj: { [index: string]: number | string } = {};
+      const obj: { [indedx: string]: number | string } = {};
       dataRecords.map((dataRecord: [number, DataField]) => {
         const data = this.processValue(
           fields[dataRecord[0]],
@@ -44,11 +44,10 @@ export class Message {
     return finished;
   }
 
-  processValue(field: Field, value: any): [string, number | string] {
-    let processedValue: number | string = 0;
-
+  // deno-lint-ignore no-explicit-any
+  processValue(field: Field, value: any): any {
     if (field["type"].substring(0, 4) === "enum") {
-      processedValue = ENUMS[field["type"]][value];
+      value = ENUMS[field["type"]][value];
     } else if (
       field["type"] === "dateTime" ||
       field["type"] === "localDateTime"
@@ -56,19 +55,33 @@ export class Message {
       const t = new Date(Date.UTC(1989, 11, 31, 0, 0, 0)).getTime() / 1000;
       const d = new Date(0);
       d.setUTCSeconds(value + t);
-      processedValue = d.toISOString();
+      value = d.toISOString();
     } else if (field["type"] === "coordinates") {
-      processedValue *= 180.0 / 2 ** 31;
+      value *= 180.0 / 2 ** 31;
+    } else {
+      console.log(field["type"]);
     }
 
     if (field["scale"] !== 0) {
-      processedValue = (value * 1.0) / field["scale"];
+      if (Array.isArray(value)) {
+        value = value.map((val) => {
+          return (val * 1.0) / field["scale"];
+        });
+      } else {
+        value = (value * 1.0) / field["scale"];
+      }
     }
 
     if (field["offset"] !== 0) {
-      processedValue = value - field["offset"];
+      if (Array.isArray(value)) {
+        value = value.map((val) => {
+          return val - field["offset"];
+        });
+      } else {
+        value = value - field["offset"];
+      }
     }
 
-    return [field["name"], processedValue];
+    return [field["name"], value];
   }
 }
