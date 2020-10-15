3e1d39b4970df4f7f61eb8dd68a3807f4b56fa3e
Jonathan Simpson <jsimpson.github@gmail.com>
2020-10-14 21:05:25 -0700

    chore: clean up dead code, clean up usage of any

--- message.ts
+++ message.ts
@@ -8,7 +8,7 @@ import { DefinitionRecord } from "./definition_record.ts";
 export class Message {
   globalMsgNum: number;
   name: string;
-  data: { [index: string]: string }[] = [];
+  data: { [index: string]: number | string }[] = [];
 
   constructor(globalMsgNum: number, definitions: DefinitionRecord[]) {
     this.globalMsgNum = globalMsgNum;
@@ -27,10 +27,10 @@ export class Message {
   makeMessage(
     fields: { [index: string]: Field },
     definition: DefinitionRecord,
-  ): { [index: string]: string }[] {
-    const finished: { [index: string]: string }[] = [];
+  ): { [index: string]: number | string }[] {
+    const finished: { [index: string]: number | string }[] = [];
     definition.valid().map((dataRecords: [number, DataField][]) => {
-      const obj: { [index: string]: string } = {};
+      const obj: { [index: string]: number | string } = {};
       dataRecords.map((dataRecord: [number, DataField]) => {
         const data = this.processValue(
           fields[dataRecord[0]],
@@ -44,9 +44,11 @@ export class Message {
     return finished;
   }
 
-  processValue(field: Field, value: any): [string, any] {
+  processValue(field: Field, value: any): [string, number | string] {
+    let processedValue: number | string = 0;
+
     if (field["type"].substring(0, 4) === "enum") {
-      value = ENUMS[field["type"]][value];
+      processedValue = ENUMS[field["type"]][value];
     } else if (
       field["type"] === "dateTime" ||
       field["type"] === "localDateTime"
@@ -54,31 +56,19 @@ export class Message {
       const t = new Date(Date.UTC(1989, 11, 31, 0, 0, 0)).getTime() / 1000;
       const d = new Date(0);
       d.setUTCSeconds(value + t);
-      value = d.toISOString();
+      processedValue = d.toISOString();
     } else if (field["type"] === "coordinates") {
-      value *= 180.0 / 2 ** 31;
+      processedValue *= 180.0 / 2 ** 31;
     }
 
     if (field["scale"] !== 0) {
-      if (Array.isArray(value)) {
-        value = value.map((val) => {
-          return (val * 1.0) / field["scale"];
-        });
-      } else {
-        value = (value * 1.0) / field["scale"];
-      }
+      processedValue = (value * 1.0) / field["scale"];
     }
 
     if (field["offset"] !== 0) {
-      if (Array.isArray(value)) {
-        value = value.map((val) => {
-          return val - field["offset"];
-        });
-      } else {
-        value = value - field["offset"];
-      }
+      processedValue = value - field["offset"];
     }
 
-    return [field["name"], value];
+    return [field["name"], processedValue];
   }
 }
