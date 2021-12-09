121bc312ad709b2264c4500b2385d3ad5bad02a9
Jonathan Simpson <jsimpson.github@gmail.com>
2021-12-09 15:23:29 -0800

    deps: update dependencies, tests

--- test_deps.ts
+++ test_deps.ts
@@ -1,4 +1,4 @@
 export const { test } = Deno;
 
-export { BinaryReader } from "https://deno.land/x/binary_reader@v0.1.4/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.65.0/testing/asserts.ts";
+export { BinaryReader } from "https://deno.land/x/binary_reader@v0.1.6/mod.ts";
+export { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
